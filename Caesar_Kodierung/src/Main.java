83161833b212b326b492ac29b20f01362ce48a2b
Simon <jim48@gmx.de>
2024-02-02 16:35:38 +0100

    asdads

--- Caesar_Kodierung/src/Main.java
+++ Caesar_Kodierung/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args)
     {
-
+        Caesar caesar = new Caesar();
+        caesar.Verschluesseln("Hello World", 4);
+        caesar.Entschluesseln("Lipps$[svph", 4);
     }
 }
